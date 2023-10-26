b15584d9855a2c59121720a297faed74bbcbb2c0
adwxith <21c05.adwaith@sjec.ac.in>
2023-10-26 16:56:57 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mysql = require('mysql');
-const add=require('path')
+const mysql = require('mysql2');
+const path = require('path'); // Changed 'add' to 'path'
 const app = express();
 
 const db = mysql.createConnection({
@@ -10,9 +10,12 @@ const db = mysql.createConnection({
   database: 'todo'
 });
 
-const publicPath = add.join(__dirname, 'public');
+// Body parsing middleware
+app.use(express.json());
+
+const publicPath = path.join(__dirname, 'public');
 app.get('/', (req, res) => {
-  res.sendFile(add.join(publicPath, 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.post('/login', (req, res) => {
@@ -25,57 +28,68 @@ app.post('/login', (req, res) => {
     }
 
     if (results.length > 0) {
-      res.redirect('/home');
       return res.status(200).json({ message: 'Successful login' });
     } else {
       res.redirect('/login');
     }
   });
-});
+});   
 
 app.post('/signup', (req, res) => {
-  const name=req.body.name
+  const name = req.body.name;
   const username = req.body.username;
   const password = req.body.password;
 
-  db.query('INSERT INTO login (name,username, password) VALUES (?,?, ?)', [name,username, password], (err, results) => {
+  db.query('INSERT INTO login (name, username, password) VALUES (?, ?, ?)', [name, username, password], (err, results) => {
     if (err) {
+      console.error('Database error:', err);
       return res.status(500).json({ message: 'Failed to register' });
     }
- 
-    res.redirect('/login');
     return res.status(200).json({ message: 'Registered successfully' });
   });
-  
 });
-app.post('/createtask',(req, res)=>{
-const taskname=req.body.taskname;
-const tasknumber=req.body.taskname
-db.query('Insert into task(tasknumber,taskname,done) values(?,?,?)' ,[tasknumber,taskname,false],(err,results)=>{
-  if(err){
-    return res.status(500).json({ message: 'Failed to create task' });
-  }
-  return res.status(200).json({ message: 'Created task successfully' });
-})
+
+app.post('/createtask', (req, res) => {
+  const taskname =   req.body.taskname;
+  const tasknumber = req.body.task_id;
+  const description = req.body.description; // Fixed typo
+
+  db.query('INSERT INTO task (task_id, taskname, description, done) VALUES (?, ?, ?, ?)', [tasknumber, taskname, description, false], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: 'Failed to create task' });
+    }
+    return res.status(200).json({ message: 'Created task successfully' });
+  });
 });
-app.post('/deletetask',(req, res)=>{
-  
-  const tasknumber=req.body.taskname
-  db.query('Delete from task where tasknumber =(?)' ,[tasknumber],(err,results)=>{
-    if(err){
+
+app.post('/deletetask', (req, res) => {
+  const tasknumber = req.body.taskname; // Changed variable name
+
+  db.query('DELETE FROM task WHERE task_id = ?', [tasknumber], (err, results) => { // Corrected the SQL query
+    if (err) {
       return res.status(500).json({ message: 'Failed to delete task' });
     }
-  })
+    return res.status(200).json({ message: 'Deleted task successfully' });
   });
-  app.post('/changestatus'),(req,res)=>{
-    const status=req.body.status;
-    db.query('Update todo set done=(?)',[status],(err,results)=>{
-      if(err){
-        return res.status(500).json({ message: 'Failed to change status' });
-      }
-      return res.status(200).json({ message: 'Changed status successfully' });
-    })
-  }
+});
+
+app.post('/changestatus', (req, res) => {
+  const status = req.body.status;
+
+  db.query('UPDATE task SET done = ? WHERE ...', [status], (err, results) => { // Specify the condition in WHERE clause
+    if (err) {
+      return res.status(500).json({ message: 'Failed to change status' });
+    }
+    return res.status(200).json({ message: 'Changed status successfully' });
+  });
+});
+app.post('/getdata',(req, res)=>{
+  const task_id=req.body.task_id;
+  db.query('Select * from task where task_id=(?)',[task_id],(err,results)=>{
+  return res.json(results);
+  });
+});
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
