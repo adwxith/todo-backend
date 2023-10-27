352e234fa5d92b4a3aca24d419672b0dcdcd8190
adwxith <21c05.adwaith@sjec.ac.in>
2023-10-27 17:34:29 +0530

    a lil logic change

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path'); // Changed 'add' to 'path'
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -24,7 +27,7 @@ app.post('/login', (req, res) => {
 
   db.query('SELECT * FROM login WHERE username = ? AND password = ?', [username, password], (err, results) => {
     if (err) {
-      return res.status(500).json({ message: 'Failed to login' });
+      return res.status(401).json({ message: 'Failed to login' });
     }
 
     if (results.length > 0) {
@@ -43,7 +46,7 @@ app.post('/signup', (req, res) => {
   db.query('INSERT INTO login (name, username, password) VALUES (?, ?, ?)', [name, username, password], (err, results) => {
     if (err) {
       console.error('Database error:', err);
-      return res.status(500).json({ message: 'Failed to register' });
+      return res.status(401).json({ message: 'Failed to register' });
     }
     return res.status(200).json({ message: 'Registered successfully' });
   });
@@ -51,38 +54,52 @@ app.post('/signup', (req, res) => {
 
 app.post('/createtask', (req, res) => {
   const taskname =   req.body.taskname;
-  const tasknumber = req.body.task_id;
-  const description = req.body.description; // Fixed typo
-
-  db.query('INSERT INTO task (task_id, taskname, description, done) VALUES (?, ?, ?, ?)', [tasknumber, taskname, description, false], (err, results) => {
+  const task_id = parseInt(req.body.task_id);
+  const description = req.body.description;
+   // Fixed typo
+  
+  db.query('INSERT INTO task (taskname, task_id, description, done) VALUES (?, ?, ?, ?)', [taskname, task_id, description, false], (err, results) => {
     if (err) {
-      return res.status(500).json({ message: 'Failed to create task' });
+      return res.status(401).json({ message: 'Failed to create task' });
     }
     return res.status(200).json({ message: 'Created task successfully' });
   });
 });
 
 app.post('/deletetask', (req, res) => {
-  const tasknumber = req.body.taskname; // Changed variable name
+  const tasknumber = parseInt(req.body.task_id); // Changed variable name
 
   db.query('DELETE FROM task WHERE task_id = ?', [tasknumber], (err, results) => { // Corrected the SQL query
     if (err) {
-      return res.status(500).json({ message: 'Failed to delete task' });
+      return res.status(401).json({ message: 'Failed to delete task' });
     }
     return res.status(200).json({ message: 'Deleted task successfully' });
   });
 });
 
 app.post('/changestatus', (req, res) => {
-  const status = req.body.status;
+  const status = req.body.done;
+  const task_id = parseInt(req.body.task_id); // Assuming task_id is an integer
 
-  db.query('UPDATE task SET done = ? WHERE ...', [status], (err, results) => { // Specify the condition in WHERE clause
-    if (err) {
-      return res.status(500).json({ message: 'Failed to change status' });
-    }
-    return res.status(200).json({ message: 'Changed status successfully' });
-  });
+  if (status === 'true') {
+    db.query('UPDATE task SET done = ? WHERE task_id = ?', [true, task_id], (err, results) => {
+      if (err) {
+        return res.status(401).json({ message: 'Failed to change status' });
+      }
+      return res.status(200).json({ message: 'Changed status successfully' });
+    });
+  } else if (status === 'false') {
+    db.query('UPDATE task SET done = ? WHERE task_id = ?', [false, task_id], (err, results) => {
+      if (err) {
+        return res.status(401).json({ message: 'Failed to change status' });
+      }
+      return res.status(200).json({ message: 'Changed status successfully' });
+    });
+  } else {
+    return res.status(400).json({ message: 'Invalid status value' });
+  }
 });
+
 app.post('/getdata',(req, res)=>{
   const task_id=req.body.task_id;
   db.query('Select * from task where task_id=(?)',[task_id],(err,results)=>{
